49b45c412eb72e66993656ecc4b785d422243076
Mutai001 <omusekim@gmail.com>
2024-10-14 18:06:11 +0300

    Footer.tsx

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="text-center p-4 bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-300">
+      © 2024 Mental Health App. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
