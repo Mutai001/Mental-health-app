770e399da9934a91d9aa498010fea332809ac399
mutai001 <omusekim@gmail.com>
2025-02-11 23:09:51 +0300

    upload working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import React from 'react';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 // import Header from './components/Header';
 import Navbar from './components/Navbar';
@@ -9,23 +8,38 @@ import Testimonials from './components/Testimonials';
 import Specialists from './components/Specialists';
 import Mindtraining from './components/Mindtraining';
 import Footer from './components/Footer';
-import ProfilePage from './components/UserDash/Profile'; // Import ProfilePage
+// import ProfilePage from './components/UserDash/Profile'; 
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div>
-        {/* <Header /> */}
-        <Navbar />
+     <div id="about">
+     <Navbar />
+     </div>
+      <div id='herosection'>
         <HeroSection />
+        </div>
+      <div id='areaofexpertise'>
         <AreaOfExpertise />
-        <CaseStudies />
-        <Testimonials />
-        <Specialists />
-        <Mindtraining />
-        <Footer />
-        <ProfilePage />
+        </div>
+      <div className="casestudies">
+      <CaseStudies />
       </div>
+      <div id='testimonials'>
+        <Testimonials />
+        </div>
+      <div id='specialists'>
+        <Specialists />
+        </div>
+      <div id='mindtraining'>
+        <Mindtraining />
+        </div>
+      <div id='footer'>
+        <Footer />
+        </div>
+      {/* <div id='profile'>
+        <ProfilePage />
+        </div> */}
     </BrowserRouter>
   );
 };
