08937553dcdbe94c95f1bdaf905f40a037a0dd54
mutai001 <omusekim@gmail.com>
2025-02-16 12:17:09 +0300

    build reolsved

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
+import commonjs from 'vite-plugin-commonjs'; // Add this
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    commonjs(), // Add this
+    visualizer({
+      open: true,
+      filename: 'bundle-analysis.html',
+    }),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          lodash: ['lodash'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+});
